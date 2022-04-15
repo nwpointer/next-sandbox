344a5ca01fac37fc7c11efdd9f5757beb34aedc8
Nathan Pointer <nathanpointer@Nathans-MacBook-Pro-2.local>
2022-04-14 21:13:38 -0700

    Initial commit

---
+++ test/next-sandbox.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const NextSandbox = require('../lib/next-sandbox-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/next-sandbox-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new NextSandbox.NextSandboxStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
